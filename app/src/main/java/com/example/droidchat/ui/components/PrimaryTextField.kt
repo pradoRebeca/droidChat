d67c81796dbd6a742270e07bee2e985392bf8b72
pradoRebeca <rebeca.prado.dev@gmail.com>
2025-09-02 13:20:08 -0300

    feat: sign in screen

--- app/src/main/java/com/example/droidchat/ui/components/PrimaryTextField.kt
+++ app/src/main/java/com/example/droidchat/ui/components/PrimaryTextField.kt
@@ -1,16 +1,79 @@
 package com.example.droidchat.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.droidchat.R
+import com.example.droidchat.ui.theme.ColorError
 
 @Composable
-fun PrimaryTextField(modifier: Modifier = Modifier) {
-    
+fun PrimaryTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            decorationBox = { innerTextField ->
+                Surface(shape = CircleShape) {
+
+                    Row(
+                        modifier = Modifier
+                            .padding(16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+                        Image(
+                            painter = painterResource(R.drawable.ic_lock),
+                            contentDescription = ""
+                        )
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Box(modifier = Modifier.weight(1f)) {
+                            innerTextField()
+                        }
+
+                        Spacer(modifier = Modifier.width(8.dp))
+
+                        Image(
+                            painter = painterResource(R.drawable.ic_lock),
+                            contentDescription = ""
+                        )
+                    }
+                }
+
+            }
+        )
+
+        Text(
+            text = "Senha obrigatoria",
+            modifier = Modifier.padding(start = 16.dp),
+            color = ColorError
+        )
+    }
+
+
 }
 
 @Preview
 @Composable
 private fun PrimaryTextFieldPreview() {
-    PrimaryTextField()
+    PrimaryTextField(value = "", onValueChange = {})
 }
--- app/src/main/java/com/example/droidchat/ui/feature/signin/SignInScreen.kt
+++ app/src/main/java/com/example/droidchat/ui/feature/signin/SignInScreen.kt
@@ -5,12 +5,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.droidchat.R
+import com.example.droidchat.ui.components.PrimaryTextField
 import com.example.droidchat.ui.theme.BackgroundGradient
 import com.example.droidchat.ui.theme.DroidChatTheme
 
@@ -31,6 +38,18 @@ fun SignInScreen() {
         Image(
             painter = painterResource(id = R.drawable.logo),
             contentDescription = "logo"
+        )
+
+        var email by remember {
+            mutableStateOf("")
+        }
+
+        PrimaryTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            modifier = Modifier.padding(horizontal = 16.dp)
         )
     }
 }
